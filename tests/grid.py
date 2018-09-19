c46127dc20905d472e00bbe9fcdf808aab4ed5f0
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2018-09-19 17:14:15 -0500

    Add Grid class For indexing into an array of characters with spatial coordinates

---
+++ tests/grid.py
@@ -0,0 +1,78 @@
+from unittest import TestCase
+from array import array
+from itertools import islice
+
+from wordsearch.grid import Coordinates, Grid
+
+class GridTest(TestCase):
+  characters = 'UMKHU' + 'LKINV' + 'JOCWE' + 'LLSHK' + 'ZZWZC'
+  grid = Grid(array('I', map(ord, characters)))
+
+  def test_index(self):
+    self.assertEqual(self.grid[(0, 0)], 'U')
+    self.assertEqual(self.grid[(4, 0)], 'U')
+
+    self.assertEqual(self.grid[(0, 1)], 'L')
+    self.assertEqual(self.grid[(4, 1)], 'V')
+
+    self.assertEqual(self.grid[(2, 2)], 'C')
+    self.assertEqual(self.grid[(3, 3)], 'H')
+
+    self.assertEqual(self.grid[(0, 4)], 'Z')
+    self.assertEqual(self.grid[(4, 4)], 'C')
+
+    with self.assertRaises(KeyError):
+      self.grid[(5, 5)]
+
+  def test_iterator(self):
+    self.assertEqual(''.join(self.grid.values()), self.characters)
+
+  def test_contains(self):
+    self.assertTrue((0, 0) in self.grid)
+    self.assertTrue((2, 2) in self.grid)
+    self.assertTrue((4, 4) in self.grid)
+
+    self.assertFalse((5, 4) in self.grid)
+    self.assertFalse((5, 5) in self.grid)
+    self.assertFalse((-1, -1) in self.grid)
+
+  def test_len(self):
+    self.assertEqual(len(self.grid), 25)
+
+  def test_size(self):
+    self.assertEqual(self.grid.size, 5)
+
+  def test_address(self):
+    self.assertEqual(self.grid.address((0, 0)), 0)
+    self.assertEqual(self.grid.address((1, 1)), 6)
+    self.assertEqual(self.grid.address((2, 2)), 12)
+    self.assertEqual(self.grid.address((3, 3)), 18)
+    self.assertEqual(self.grid.address((4, 4)), 24)
+
+  def test_coordinates(self):
+    self.assertEqual(self.grid.coordinates(0), (0, 0))
+    self.assertEqual(self.grid.coordinates(6), (1, 1))
+    self.assertEqual(self.grid.coordinates(12), (2, 2))
+    self.assertEqual(self.grid.coordinates(18), (3, 3))
+    self.assertEqual(self.grid.coordinates(24), (4, 4))
+
+  def test_vector(self):
+    # Diagonal down and right
+    first, *_, last = self.grid.vector((0, 0), 1, 1)
+    self.assertEqual(first, (0, 0))
+    self.assertEqual(last, (4, 4))
+
+    # Straight right
+    first, *_, last = self.grid.vector((0, 2), 1, 0)
+    self.assertEqual(first, (0, 2))
+    self.assertEqual(last, (4, 2))
+
+    # Diagonal up and left
+    first, *_, last = self.grid.vector((4, 4), -1, -1)
+    self.assertEqual(first, (4, 4))
+    self.assertEqual(last, (0, 0))
+
+    # Straight up
+    first, *_, last = self.grid.vector((2, 4), 0, -1)
+    self.assertEqual(first, (2, 4))
+    self.assertEqual(last, (2, 0))
---
+++ wordsearch/grid.py
@@ -0,0 +1,86 @@
+from array import array
+from math import sqrt
+from itertools import takewhile
+from collections.abc import Mapping
+from typing import Iterator, Tuple, cast
+
+Coordinates = Tuple[int, int]
+Vector = Iterator[Coordinates]
+
+def vector(horizontal: int, vertical: int, start=(0, 0)) -> Vector:
+  """Generates points from a starting position in a constant direction"""
+  x, y = start
+
+  while True:
+    yield (x, y)
+    x += horizontal
+    y += vertical
+
+class Grid(Mapping):
+  """
+  An abstraction to represent a puzzle grid. Grid wraps a single dimension array of characters
+  and maps between array addresses and grid coordinate pairs.
+
+  This class allows indexing into the character array based on (column, row) coordinate pairs. This is
+  convenient for traversing the grid in two dimensions to search for words. See the `vector` method.
+  """
+
+  characters: array
+
+  def __init__(self, characters: array) -> None:
+    self.characters = characters
+
+  def __getitem__(self, coordinates: Coordinates) -> str:
+    """Allows indexing of the character array by grid coordinates"""
+    if coordinates not in self:
+      raise KeyError(f'Coordinate pair {coordinates} outside of grid extent')
+
+    i = self.address(coordinates)
+    return chr(self.characters[i])
+
+  def __iter__(self) -> Iterator[Coordinates]:
+    """Return an iterator over all points in the grid"""
+    return map(self.coordinates, range(len(self)))
+
+  def __len__(self) -> int:
+    """Return the number of characters in the grid"""
+    return len(self.characters)
+
+  def __contains__(self, key: object) -> bool:
+    """Returns a boolean indicating whether a coordinate pair lies within the grid extent"""
+    if not isinstance(key, tuple):
+      return False
+
+    x, y = key
+
+    if not (isinstance(x, int) and isinstance(y, int)):
+      return False
+
+    size = self.size
+
+    return (0 <= x < size) and (0 <= y < size)
+
+  @property
+  def size(self) -> int:
+    """Compute the size (width = height) of the grid square"""
+    return int(sqrt(len(self.characters)))
+
+  def address(self, coordinates: Coordinates) -> int:
+    """Calculate the array address for a given coordinate pair using row major order"""
+    column, row = coordinates
+    return self.size * row + column
+
+  def coordinates(self, address: int) -> Coordinates:
+    """Calculate the point coordinates of a given address using row major order"""
+    size = self.size
+    row = address // size
+    column = address % size
+
+    return (column, row)
+
+  def vector(self, start: Coordinates, horizontal: int, vertical: int) -> Vector:
+    """
+    Generate a vector of coordinates from the grid based on a starting position and constant direction.
+    The vector terminates at the grid extent and does not wrap.
+    """
+    return takewhile(self.__contains__, vector(horizontal, vertical, start))
